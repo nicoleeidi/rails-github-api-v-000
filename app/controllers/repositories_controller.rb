5239c946fa49ba7689b5aa5d90c4508631b54d7e
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-19 18:40:57 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,7 +4,7 @@ class RepositoriesController < ApplicationController
   #def index
 
     def index
-  response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token 1", 'Accept' => 'application/json'}
+  response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
 
   @repos = JSON.parse(response.body)
 end
